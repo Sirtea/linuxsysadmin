756c0ed496b5497d47dba2ca675879ffb8385b9b
Gerard Monells <gerard.monells@gmail.com>
2015-10-05 23:39:19 +0200

    FIX: site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://linuxsysadmin.tk'
+SITEURL = 'http://www.linuxsysadmin.tk'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
