a33fa8960d988da5c6bcbf73f58176339a396651
Gerard Monells Bort <gerard.monells@amaris.com>
2015-10-06 12:35:58 +0200

    Analytics y diqus

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "linuxsysadmin-gmb"
+GOOGLE_ANALYTICS = "UA-68486572-1"
